c2170627870bbdac61ccf9e23dc6b6bc7f36c6eb
Sarpu Khokhar <your-email@example.com>
2025-04-29 15:40:55 +0530

    Update for Render static deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 👈 ye line add karo for static export
 };
 
 export default nextConfig;
